6a57a107ed4b217f6fea0e9ea7f1235fd10d81ed
Michael Smith <michael@eightnine.co>
2018-05-05 14:36:47 -0500

    Update linking logic
    "See It All" link now links to the active/selected chapter if the user
    is in a view with a selected chapter

--- src/Components/Nav/NavMenu.js
+++ src/Components/Nav/NavMenu.js
@@ -13,9 +13,22 @@ import './nav.scss';
 const NavMenu = () => {
   // This component exists outside of the Router's `<Switch>` case
   // So window.location.pathname is used instead of `withRouter` props
-  const navClass = window.location.pathname === '/'
+  const { location: { pathname } } = window;
+  const navClass = pathname === '/'
     ? 'nav--is-splash'
     : '';
+
+  // By default the current chapter is 0 (first chapter)
+  let currentChapter = 0;
+  // If we're in a content stream page (grid or detail views)
+  // Then slice out the current chapter from the URL by pulling out the character present
+  // immediately after `/content-stream/`, which is the URL param for `chapterId`
+  // NOTE: This will break in the future if we have 10+ (double digit) chapter IDs
+  if (pathname.includes('/content-stream/')) {
+    const start = pathname.indexOf('/content-stream/') + '/content-stream/'.length;
+    const end = start + 1;
+    currentChapter = pathname.slice(start, end);
+  }
 
   return (
     <nav className={`nav ${navClass}`}>
@@ -24,7 +37,7 @@ const NavMenu = () => {
         <li className="nav__link">
           <NavLink
             exact
-            to="/content-stream/0"
+            to={`/content-stream/${currentChapter}`}
             activeClassName="nav-link--active"
           >
             See-It-All
