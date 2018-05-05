08e46c213b9ff08a0ea17a579a63e207cef99195
Michael Smith <michael@eightnine.co>
2018-05-05 11:05:46 -0500

    New data structure for pagination

--- src/Data/streamData.js
+++ src/Data/streamData.js
@@ -1,122 +1,129 @@
 export default [
-  {
-    image: "cover the world",
-    title: "cover the world",
-    subtitle: `we aim to “cover the earth” aim to cover "the world", the rest doesn't exist`
-  },
-  {
-    image: "people cry",
-    title: "people cry",
-    subtitle: `shed tears, especially as an expression of distress or pain. "don't cry—it'll be all right"`
-  },
-  {
-    image: "cheree",
-    title: "cheree cheree",
-    subtitle: "teardrops on me, myself, I always thought you were pretty in a red dress"
-  },
-  {
-    image: "cross2bear",
-    title: "snake oil",
-    subtitle: '"her shoulders were as white as marble", n the marble holds her still'
-  },
-  {
-    image: "im yr dog",
-    title: 'ich lüge',
-    subtitle: "I've been crazy for this boy I really like his gun—I've got nowhere to run"
-  },
-  {
-    image: "left my heart alone",
-    title: "glasshouse",
-    subtitle: "buried my heart inna wounded creek last time I saw my baby n I ain't seen it since"
-  },
-  {
-    image: "15set",
-    title: "15set",
-    subtitle: "on one side of the school there is a set of 20 stairs known simply as El Toro 20 (-5)"
-  },
-  {
-    image: "letter from the sea",
-    title: "deep2",
-    subtitle: "set one's heart on, be dying for—sent the letter from the sea"
-  },
-  {
-    image: "walled garden",
-    title: "dellamorte dellamore",
-    subtitle: "to live inside the walled garden till its good to get out"
-  },
-  {
-    image: "snake eights",
-    title: "oregon vice",
-    subtitle: "out at the tracks won't someone tell her father how much we miss her"
-  },
-  {
-    image: "soupbowl",
-    title: "been out",
-    subtitle: "traditional combination, less than the three but don't be scared—gets you right anyway"
-  },
-  {
-    image: "heirloom",
-    title: "heir(s)",
-    subtitle: "the only living person who even calls me (you can't live on memories alone)"
-  },
-  {
-    image: "yea lime",
-    title: "estereo",
-    subtitle: "sometimes I get so drunk it's all I feel, or rewards for waking up"
-  },
-  {
-    image: "army man",
-    title: "decrease yr options",
-    subtitle: "illegally leaving the armed forces (her mother's desertion of her family)"
-  },
-  {
-    image: "corner pocket",
-    title: "$90 layup",
-    subtitle: "possible answers are affirmative, five are negative, and five are non-committal"
-  },
-  {
-    image: "coughinnails",
-    title: "french tips",
-    subtitle: "why don't you just come back to us? (I don't think I can get it out of me anymore)"
-  },
-  {
-    image: "urn",
-    title: "nightvision",
-    subtitle: "why the barbed wire? Do they climb in at night? No, they climb out."
-  },
-  {
-    image: "waterfall",
-    title: "premonition",
-    subtitle: "(looking disturbed) I don't think we survived that crash—how'd you guess?"
-  },
-  {
-    image: "tears",
-    title: "do svidaniya",
-    subtitle: "pull or rip (something) apart or to pieces with force (to produce tears)"
-  },
-  {
-    image: "koolaid",
-    title: "x-ray",
-    subtitle: "deeper than the blood I'm submerged in, black checkered floor with the red velvet curtain"
-  },
-  {
-    image: "fish soup",
-    title: "fish soup",
-    subtitle: "partook in the ritual (never even noticed) till I realized a pause in the decay"
-  },
-  {
-    image: "matchbox tears",
-    title: "altered state",
-    subtitle: "I was never real to him. (crying) Nothing in the human experience is real to him."
-  },
-  {
-    image: "red vase",
-    title: "collection urn",
-    subtitle: `this one he wrote in cold blood with a toothpick...he said "True" through acoustics`
-  },
-  {
-    image: "beetljuic",
-    title: "beetljuic",
-    subtitle: "when the cup overflows—death's in your blood, fear's all in your head"
-  }
+  // PAGE 1 (Red & Blue drawings)
+  [
+
+  ],
+  // PAGE 2 (Black lined drawings)
+  [
+    {
+      image: "cover the world",
+      title: "cover the world",
+      subtitle: `we aim to “cover the earth” aim to cover "the world", the rest doesn't exist`
+    },
+    {
+      image: "people cry",
+      title: "people cry",
+      subtitle: `shed tears, especially as an expression of distress or pain. "don't cry—it'll be all right"`
+    },
+    {
+      image: "cheree",
+      title: "cheree cheree",
+      subtitle: "teardrops on me, myself, I always thought you were pretty in a red dress"
+    },
+    {
+      image: "cross2bear",
+      title: "snake oil",
+      subtitle: '"her shoulders were as white as marble", n the marble holds her still'
+    },
+    {
+      image: "im yr dog",
+      title: 'ich lüge',
+      subtitle: "I've been crazy for this boy I really like his gun—I've got nowhere to run"
+    },
+    {
+      image: "left my heart alone",
+      title: "glasshouse",
+      subtitle: "buried my heart inna wounded creek last time I saw my baby n I ain't seen it since"
+    },
+    {
+      image: "15set",
+      title: "15set",
+      subtitle: "on one side of the school there is a set of 20 stairs known simply as El Toro 20 (-5)"
+    },
+    {
+      image: "letter from the sea",
+      title: "deep2",
+      subtitle: "set one's heart on, be dying for—sent the letter from the sea"
+    },
+    {
+      image: "walled garden",
+      title: "dellamorte dellamore",
+      subtitle: "to live inside the walled garden till its good to get out"
+    },
+    {
+      image: "snake eights",
+      title: "oregon vice",
+      subtitle: "out at the tracks won't someone tell her father how much we miss her"
+    },
+    {
+      image: "soupbowl",
+      title: "been out",
+      subtitle: "traditional combination, less than the three but don't be scared—gets you right anyway"
+    },
+    {
+      image: "heirloom",
+      title: "heir(s)",
+      subtitle: "the only living person who even calls me (you can't live on memories alone)"
+    },
+    {
+      image: "yea lime",
+      title: "estereo",
+      subtitle: "sometimes I get so drunk it's all I feel, or rewards for waking up"
+    },
+    {
+      image: "army man",
+      title: "decrease yr options",
+      subtitle: "illegally leaving the armed forces (her mother's desertion of her family)"
+    },
+    {
+      image: "corner pocket",
+      title: "$90 layup",
+      subtitle: "possible answers are affirmative, five are negative, and five are non-committal"
+    },
+    {
+      image: "coughinnails",
+      title: "french tips",
+      subtitle: "why don't you just come back to us? (I don't think I can get it out of me anymore)"
+    },
+    {
+      image: "urn",
+      title: "nightvision",
+      subtitle: "why the barbed wire? Do they climb in at night? No, they climb out."
+    },
+    {
+      image: "waterfall",
+      title: "premonition",
+      subtitle: "(looking disturbed) I don't think we survived that crash—how'd you guess?"
+    },
+    {
+      image: "tears",
+      title: "do svidaniya",
+      subtitle: "pull or rip (something) apart or to pieces with force (to produce tears)"
+    },
+    {
+      image: "koolaid",
+      title: "x-ray",
+      subtitle: "deeper than the blood I'm submerged in, black checkered floor with the red velvet curtain"
+    },
+    {
+      image: "fish soup",
+      title: "fish soup",
+      subtitle: "partook in the ritual (never even noticed) till I realized a pause in the decay"
+    },
+    {
+      image: "matchbox tears",
+      title: "altered state",
+      subtitle: "I was never real to him. (crying) Nothing in the human experience is real to him."
+    },
+    {
+      image: "red vase",
+      title: "collection urn",
+      subtitle: `this one he wrote in cold blood with a toothpick...he said "True" through acoustics`
+    },
+    {
+      image: "beetljuic",
+      title: "beetljuic",
+      subtitle: "when the cup overflows—death's in your blood, fear's all in your head"
+    }
+  ]
 ]
