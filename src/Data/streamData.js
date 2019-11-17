506bf107daa98aa9eaeda808a86d793bac538305
Michael Smith <michael@eightnine.co>
2019-11-17 13:59:46 -0600

    Final poem additions

--- src/Data/streamData.js
+++ src/Data/streamData.js
@@ -3,7 +3,7 @@ export default [
   {
     meta: {
       title: 'prayer card vendor',
-      subtitle: `aiding in: unhappiness, release of fear, insomnia, mysteries unveiled... a gift to die where you're from`,
+      subtitle: `aiding in: unhappiness, release of fear, insomnia, mysteries unveiled... a gift to die where you're from n we plan to collect`,
       media: 'chapter-1-transition',
       mediaType: '.gif'
     },
@@ -24,11 +24,11 @@ export default [
         image: 'kitana',
         title: 'outerworld',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "traveled all this way just to wish i'd left my bags at home-give me a reason to go back"
       },
       {
         image: 'hellraiser',
-        title: 'real speed type',
+        title: 'a real speed type',
         subtitle:
           "don't you see (we) want to kill you a thousand times, to the limits of eternity, if eternity could have limits?"
       },
@@ -36,116 +36,108 @@ export default [
         image: 'pursuit-special',
         title: 'pursuit special',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "the chain in those handcuffs is high-tensile steel, hotter than a rollin dice. it'd take you ten minutes "
       },
       {
         image: 'sun-sets',
-        title: 'example title..',
+        title: 'sun2snowblind',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "the sun smiles while his brother dies—can't say it gets reciprocated, tears fall just to one side"
       },
       {
         image: 'bell-of-awakening',
-        title: 'example title..',
+        title: 'bell of awakening',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "chosen one, huh? quite the trip through low frame towns, would have given up if i was you. can't say its wrong"
       },
       {
         image: 'born-star',
         title: 'born star',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          'born like this, made like this—destined for this. cost benefit had me leaning towards i just might have to'
       },
       {
         image: 'cemetery-sky',
         title: 'life outside',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "never felt that way about a man before, hands on my own throat. i've definitely thought about killing you"
       },
       {
         image: 'no-mans-land',
-        title: 'example title..',
+        title: 'land ov myne',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "we had to leave em... no chance for none of us it we aint, can't say i minded, couldn't say i had time"
       },
       {
         image: 'show-lights',
         title: 'streetlight',
-        subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+        subtitle: `big time, they said its the "big time". can't go wrong now, impossible the tide won't roll back yet`
       },
       {
         image: 'slot-machine',
         title: 'consolation prize',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "winner winner winner winner winner winner, we ain't never hearing that again i promised"
       },
       {
         image: 'night-watch',
         title: 'night watch',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "it was my job, not that it still ain't, don't care about how much i do it... no one seemed to mind"
       },
       {
         image: 'trapdoor',
         title: 'drowning desire',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "said i'd let her kill me if she did, ain't never been one to put down another on account of taste"
       },
       {
         image: 'halloween-garden',
-        title: 'example title..',
+        title: 'memorial bouquet',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "ain't much of a condolence, hearin bout missin me... always been here if you wanted to visit"
       },
       {
         image: 'western-depression',
         title: 'western depression',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
-      },
-      {
-        image: 'sacrifice',
-        title: 'example title..',
-        subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          'left me the ranch house as a parting gift, still aint moved out (more a curse n i think he knew that)'
       },
       {
         image: 'inner-garden',
         title: 'interior garden',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "ain't gonna let you in baby, i know how cold it is out there but trust me it ain't better inside"
       },
       {
         image: 'old-school',
-        title: '2013 face reveal',
+        title: 'face of 2013',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "look back its aight to hate your work, you've made it when you learn to start hating it immediately"
       },
       {
         image: 'main-stage',
-        title: 'our main stage',
+        title: 'main stage',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "a place to rest your head, relaxation, enjoyment... seen yourself differently when you're done"
       },
       {
         image: 'hotknive',
         title: 'hotknive',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          'breathin (not) outta love for respiration. nothing says forever like long term effects'
       },
-
+      {
+        image: 'homestead',
+        title: 'homestead',
+        subtitle:
+          'some people come here to visit (i live here every day) n chances are i probably will'
+      },
       {
         image: 'scorpion-king',
         title: 'example title..',
         subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
-      },
-      {
-        image: 'homestead',
-        title: 'homestea',
-        subtitle:
-          "you are seven years old. you are a man. bury your first toy and your mother's picture"
+          "to gomorrah. nothing we can say will stop him (not sure i'd want to) glory left for me"
       }
     ]
   },
